48cb64afd851df69d78922463d3759e37d63f16f
moberacker <markus.oberacker@ptv.de>
2016-04-07 16:49:26 +0200

    added test project

---
+++ Log4IoTHubTest/LoggerTests.cs
@@ -0,0 +1,24 @@
+﻿using log4net;
+using System;
+
+namespace Log4IoTHubTest
+{
+    class LoggerTests
+    {
+
+        private static ILog iotHubLogger = LogManager.GetLogger(typeof(LoggerTests));
+
+        static void Main(string[] args)
+        {
+ 
+            for (int i = 0; i < 2000; i++)
+            {
+                iotHubLogger.Info(new { id = $"mob-{i}", message = $"hallo-{i}" });
+
+            }
+
+
+            System.Threading.Thread.Sleep(new TimeSpan(0, 10, 0));
+        }
+    }
+}
